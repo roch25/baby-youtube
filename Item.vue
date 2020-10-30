0a41702b75fd330a7c0f6e4311ae7d6b684d675a
git-roch <rochellemartins25@gmail.com>
2020-10-31 01:07:26 +0530

    display the search list

--- Item.vue
+++ Item.vue
@@ -1,5 +1,20 @@
 <template>
-
+    <div class="item">
+        <div class="image">
+            <slot name="image" />
+        </div>
+        <div class="right">
+             <div class="title">
+                <slot name="title" />
+            </div>
+            <div class="channel">
+                <slot name="channel" />
+            </div>
+            <div class="description">
+                <slot name="description" />
+            </div>
+        </div> 
+  </div>
 </template>
 
 <script>
@@ -9,5 +24,32 @@ export default {
 </script>
 
 <style>
-
+    .item {
+        display: flex;
+        margin: 20px;
+        cursor: pointer;
+        border-bottom: 1px solid silver;
+        padding: 10px;
+        font-family: Arial, Helvetica, sans-serif;
+        max-width: 65%;
+    }
+    .right {
+        margin-left: 15px;
+        display: flex;
+        flex-direction: column;
+    }
+    .title{
+        font-size: 4.2vmin;
+        justify-self: flex-start;
+        font-weight: 700;
+    }
+    .channel{
+        font-size: 3.8vmin;
+        justify-self: left;
+        margin-top: 12px;
+    }
+    .description{
+        margin-top: 12px;
+        font-size: 2.6vmin;
+    }
 </style>
--- List.vue
+++ List.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="list">
-      <list-item/>
+      <list-item v-for="item in searchResults" :key="item.id.videoId">
+           <template v-slot:image>
+                <img :src="item.snippet.thumbnails.medium.url"/>
+            </template>
+            <template v-slot:title>
+                {{item.snippet.title}}
+            </template>
+            <template v-slot:channel>
+                {{item.snippet.channelTitle}}
+            </template>
+             <template v-slot:description>
+                {{item.snippet.description}}
+            </template>
+      </list-item>
   </div>
 </template>
 
 <script>
 import ListItem from './Item.vue'
 export default {
+    props:{
+        searchResults:{
+            type: Array
+        }
+    },
     components:{
         ListItem
     }
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
